29cee94c936bff45c355f4989c81d2ddc1b46d34
KamileKortz <kortzkamile@gmail.com>
2026-02-25 10:39:09 -0300

    Amo o Hudson

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-function mostrarMensagem() {
-    alert("Eu te odeio");
+// Controles da Primeira Janela (Botão "Que?")
+function abrirModal1() {
+  document.getElementById("modal1").style.display = "block";
+}
+function fecharModal1() {
+  document.getElementById("modal1").style.display = "none";
 }
 
-function abrirModal() {
-  document.getElementById("meuModal").style.display = "block";
+// Controles da Segunda Janela (Botão "Eu também!")
+function abrirModal2() {
+  document.getElementById("modal2").style.display = "block";
+}
+function fecharModal2() {
+  document.getElementById("modal2").style.display = "none";
 }
 
-function fecharModal() {
-  document.getElementById("meuModal").style.display = "none";
-}
+// Fecha qualquer uma das janelas se clicar no fundo escuro
+window.onclick = function (event) {
+  let m1 = document.getElementById("modal1");
+  let m2 = document.getElementById("modal2");
 
-// Fecha se o usuário clicar fora da imagem (no fundo escuro)
-window.onclick = function(event) {
-  let modal = document.getElementById("meuModal");
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == m1) {
+    m1.style.display = "none";
+  }
+  if (event.target == m2) {
+    m2.style.display = "none";
   }
 }
