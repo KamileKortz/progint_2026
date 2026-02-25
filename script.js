e429fc560ab14a805174514993eecca44cdd3f9f
KamileKortz <kortzkamile@gmail.com>
2026-02-25 08:13:37 -0300

    HUDSON

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function mostrarMensagem() {
-    alert("Funcionandoo");
+    alert("Amoo");
+}
+
+function abrirModal() {
+  document.getElementById("meuModal").style.display = "block";
+}
+
+function fecharModal() {
+  document.getElementById("meuModal").style.display = "none";
+}
+
+// Fecha se o usuário clicar fora da imagem (no fundo escuro)
+window.onclick = function(event) {
+  let modal = document.getElementById("meuModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
